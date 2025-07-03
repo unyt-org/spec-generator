a996ecd0f264caba2f081642e88643e150947b66
Marvin Klein <MarvinsMC@yahoo.de>
2025-07-03 12:14:38 +0200

    image viewer added

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -5,6 +5,10 @@ import DefaultTheme from 'vitepress/theme'
 import './style.css'
 import TypeScriptPlayground from '../components/TypeScriptPlayground.vue'
 // import DatexPlayground from '../components/DatexPlayground.vue'
+import 'viewerjs/dist/viewer.min.css'
+import imageViewer from 'vitepress-plugin-image-viewer'
+import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue'
+import { useRoute } from 'vitepress'
 
 export default {
   extends: DefaultTheme,
@@ -13,8 +17,13 @@ export default {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
     })
   },
-  enhanceApp({ app, router, siteData }) {
+  enhanceApp({ app }) {
     app.component('TypeScriptPlayground', TypeScriptPlayground)
+    app.component('vImageViewer', vImageViewer)
     // app.component('DatexPlayground', DatexPlayground)
+  },
+  setup() {
+    const route = useRoute()
+    imageViewer(route)
   }
 } satisfies Theme
