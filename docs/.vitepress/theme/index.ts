b0c7f52209588663f67860d1788fa0d33f2f9054
Marvin Klein <MarvinsMC@yahoo.de>
2025-07-17 14:51:45 +0200

    .

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -14,11 +14,9 @@ import { useRoute, useData } from 'vitepress'
 import codeblocksFold from 'vitepress-plugin-codeblocks-fold'
 import 'vitepress-plugin-codeblocks-fold/style/index.css'
 import "vitepress-markdown-timeline/dist/theme/index.css";
+import { registerSW } from 'virtual:pwa-register'
 
-if (typeof window !== 'undefined') {
-  const { registerSW } = await import('virtual:pwa-register');
-  registerSW({ immediate: true });
-}
+registerSW({ immediate: true })
 
 if (typeof window !== 'undefined') {
   import('https://esm.sh/@unyt/datex@0.0.4')
